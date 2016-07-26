edfdc4022ed37d033dca487eb480fc6f5a23c965
iboalali <iboalali@gmail.com>
2016-07-26 22:25:07 +0200

    - Preparing the class structure for chrome. - Added a utility class.

---
+++ AppLauncherForChrome/Chrome.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppLauncherForChrome {
+    static class Chrome {
+
+        static Chrome () {
+            ExePath = Utils.GetPathForExe( "chrome.exe" );
+            User = new List<string>();
+            UserName = new List<string>();
+        }
+
+        /// <summary>
+        /// Returns the full path of the chrome executable
+        /// </summary>
+        public static string ExePath { get; private set; }
+
+        /// <summary>
+        /// Returns the full path of chrome's user data folder
+        /// </summary>
+        public static string UserDataPath
+        {
+            get
+            {
+                return System.IO.Path.Combine(
+                    Environment.GetFolderPath( Environment.SpecialFolder.LocalApplicationData ),
+                    String.Format( "Google\\Chrome\\User Data\\" ) );
+            }
+        }
+
+        /// <summary>
+        /// Contains the list of all of chrome's user profile names
+        /// </summary>
+        public static List<string> User { get; private set; }
+
+        /// <summary>
+        /// Contains the list of all of chrome's user names
+        /// </summary>
+        public static List<string> UserName { get; private set; }
+
+    }
+}
---
+++ AppLauncherForChrome/Utils.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppLauncherForChrome {
+    static class Utils {
+        private const string keyBase = @"SOFTWARE\Microsoft\Windows\CurrentVersion\App Paths";
+        public static string GetPathForExe ( string fileName ) {
+            RegistryKey localMachine = Registry.LocalMachine;
+            RegistryKey fileKey = localMachine.OpenSubKey(string.Format(@"{0}\{1}", keyBase, fileName));
+            object result = null;
+            if ( fileKey != null ) {
+                result = fileKey.GetValue( string.Empty );
+            }
+            fileKey.Close();
+
+            return ( string ) result;
+        }
+    }
+}
